f2ba89e93a86ef27cc984c1aa308a4fbb4fc0841
Roman Klein <web@romanklein.com>
2018-01-29 18:50:41 +0100

    Version 1.2.0
    - Code cleanup & reformatting
    - PHP 7.0 compatibility
    - Symphony 2.6 & 2.7 compatibility
    - Added field settings validation

--- fields/field.incremental_number.php
+++ fields/field.incremental_number.php
@@ -1,147 +1,299 @@
 <?php
 
-	Class fieldincremental_number extends Field
-	{
+if (!defined('__IN_SYMPHONY__')) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
 
-		function __construct(){
-			parent::__construct();
+class fieldincremental_number extends Field
+{
 
-			$this->_name = 'Incremental Number';
-			$this->_required = true;
-			$this->set('required', 'yes');
-		}
+    /*-------------------------------------------------------------------------
+        DEFINITION
+    -------------------------------------------------------------------------*/
 
-		function isSortable(){
-			return true;
-		}
+    /**
+     *
+     * construct
+     *
+     * @since version 1.0.0
+     */
 
-		function canFilter(){
-			return true;
-		}
+    function __construct()
+    {
+        // call the parent constructor
+        parent::__construct();
 
-		function allowDatasourceParamOutput(){
-			return true;
-		}
+        // set the name of the field
+        $this->_name = 'Incremental Number';
 
-		function canPrePopulate(){
-			return true;
-		}
+        // force "required"-setting
+        $this->_required = true;
+        $this->set('required', 'yes');
+    }
 
-		public function displaySettingsPanel(&$wrapper, $errors = NULL){
-			parent::displaySettingsPanel($wrapper, $errors);
+    /**
+     *
+     * create table
+     *
+     * @since version 1.0.0
+     */
 
-			$label = Widget::Label(__('Start Number'));
-			$label->appendChild(WIDGET::Input('fields['.$this->get('sortorder').'][start_number]', $this->get('start_number')));
-			$wrapper->appendChild($label);
-			$this->appendShowColumnCheckbox($wrapper);
-		}
+    public function createTable()
+    {
+        return Symphony::Database()->query(
+            "CREATE TABLE IF NOT EXISTS `tbl_entries_data_".$this->get('id')."` (
+                `id` int(11) unsigned NOT NULL auto_increment,
+                `entry_id` int(11) unsigned NOT NULL,
+                `value` int(11) default NULL,
+                PRIMARY KEY  (`id`),
+                KEY `entry_id` (`entry_id`),
+                KEY `value` (`value`)
+            ) TYPE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+        );
+    }
 
-		public function displayPublishPanel(&$wrapper, $data = NULL, $flagWithError = NULL, $fieldnamePrefix = NULL, $fieldnamePostfix = NULL){
-			$value = $data['value'];
-			$label = Widget::Label($this->get('label'));
+    function isSortable()
+    {
+        return true;
+    }
 
-			$label->appendChild(
-				Widget::Input(
-					'fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix,
-					(string) (strlen($value) != 0 ? $value : $this->getNewNumber()),
-					'text',
-					array('readonly' => 'readonly')
-				)
-			);
+    function canFilter()
+    {
+        return true;
+    }
 
-			if( $flagWithError != NULL ) $wrapper->appendChild(Widget::Error($label, $flagWithError));
-			else $wrapper->appendChild($label);
-		}
+    function allowDatasourceParamOutput()
+    {
+        return true;
+    }
 
-		public function processRawFieldData($data, &$status, $message, $simulate = false, $entry_id = null){
-			if( !$data ) $data = $this->getNewNumber();
+    function canPrePopulate()
+    {
+        return true;
+    }
 
-			return parent::processRawFieldData($data, $status, $message, $simulate, $entry_id);
-		}
+    function canToggle()
+    {
+        return false;
+    }
 
-		public function getNewNumber(){
-			$last_num = Symphony::Database()->fetch("
-				SELECT `value`
-				FROM `tbl_entries_data_".$this->get('id')."`
-				ORDER BY `value` DESC LIMIT 1
-			");
+    /*-------------------------------------------------------------------------
+        SETTINGS
+    -------------------------------------------------------------------------*/
 
-			return (int) (!empty($last_num)) ? $last_num[0]['value'] + 1 : $this->get('start_number');
-		}
+    /**
+     *
+     * display settings panel
+     *
+     * @since version 1.0.0
+     */
 
-		public function commit(){
-			if( !parent::commit() ) return false;
+    public function displaySettingsPanel(XMLElement &$wrapper, $errors = NULL)
+    {
+        parent::displaySettingsPanel($wrapper, $errors);
 
-			$id = $this->get('id');
-			$value = $this->get('start_number');
+        // input "start number"
 
-			if( $id === false ) return false;
+        $label = Widget::Label(__('Start Number <i>(Must be a natural number)</i>'));
+        $label->appendChild(WIDGET::Input('fields['.$this->get('sortorder').'][start_number]', $this->get('start_number')));
+        if (isset($errors['start_number'])) {
+            $wrapper->appendChild(Widget::Error($label, $errors['start_number']));
+        } else {
+            $wrapper->appendChild($label);
+        }
 
-			$fields = array();
+        $this->appendShowColumnCheckbox($wrapper);
+    }
 
-			$fields['field_id'] = $id;
-			$fields['start_number'] = $value;
+    /**
+     *
+     * check fields
+     *
+     * validate the fields settings and return errors if wrong or missing input is detected
+     *
+     * @since version 1.2.0
+     */
 
-			Symphony::Database()->query("DELETE FROM `tbl_fields_".$this->handle()."` WHERE `field_id` = '$id' LIMIT 1");
+    public function checkFields(Array &$errors, $checkForDuplicates = true)
+    {
+        // check if general field data is missing (label, handle)
 
-			return Symphony::Database()->insert($fields, 'tbl_fields_'.$this->handle());
+        $parent = parent::checkFields($errors, $checkForDuplicates);
+        if ($parent != self::__OK__) return $parent;
 
-		}
+        // build array of input data that needs to be validated by the extension
 
-		public function displayDatasourceFilterPanel(&$wrapper, $data = NULL, $errors = NULL, $fieldnamePrefix = NULL, $fieldnamePostfix = NULL){
-			$wrapper->appendChild(new XMLElement('h4', $this->get('label').' <i>'.$this->Name().'</i>'));
-			$label = Widget::Label('Value');
-			$label->appendChild(Widget::Input('fields[filter]'.($fieldnamePrefix ? '['.$fieldnamePrefix.']' : '').'['.$this->get('id').']'.($fieldnamePostfix ? '['.$fieldnamePostfix.']' : ''), ($data ? General::sanitize($data) : NULL)));
-			$wrapper->appendChild($label);
+        $check = array();
+        $check['start_number'] = $this->get('start_number');
 
-			$wrapper->appendChild(new XMLElement('p', 'To filter by ranges, add <code>mysql:</code> to the beginning of the filter input. Use <code>value</code> for field name. E.G. <code>mysql: value &gt;= 1.01 AND value &lt;= {$price}</code>', array('class' => 'help')));
+        // validate start number (must be a natural number)
 
-		}
+        if(!preg_match('/^[0-9]+$/', $check['start_number'])) {
+            $errors['start_number'] = __('Start Number must be a natural number. e.g. <code>0</code> or <code>1</code>');
+        }
 
-		public function checkPostFieldData($data, &$message, $entry_id = NULL){
-			$message = NULL;
+        return (!empty($errors) ? self::__ERROR__ : self::__OK__);
+    }
 
-			if( $this->get('required') == 'yes' && strlen($data) == 0 ){
-				$message = 'This is a required field.';
-				return self::__MISSING_FIELDS__;
-			}
+    /**
+     *
+     * commit field settings
+     *
+     * @since version 1.0.0
+     */
 
-			if( strlen($data) > 0 && !is_numeric($data) ){
-				$message = 'Must be a number.';
-				return self::__INVALID_FIELDS__;
-			}
+    public function commit()
+    {
+        if( !parent::commit() ) return false;
 
-			return self::__OK__;
-		}
+        $id = $this->get('id');
+        if( $id === false ) return false;
 
-		public function createTable(){
-			return Symphony::Database()->query(
-				"CREATE TABLE IF NOT EXISTS `tbl_entries_data_".$this->get('id')."` (
-				  `id` int(11) unsigned NOT NULL auto_increment,
-				  `entry_id` int(11) unsigned NOT NULL,
-				  `value` int(11) default NULL,
-				  PRIMARY KEY  (`id`),
-				  KEY `entry_id` (`entry_id`),
-				  KEY `value` (`value`)
-				) TYPE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+        $fields = array();
+        $fields['field_id'] = $id;
+        $fields['start_number'] = $this->get('start_number');
 
-			);
-		}
+        Symphony::Database()->query("DELETE FROM `tbl_fields_".$this->handle()."` WHERE `field_id` = '$id' LIMIT 1");
 
-		function buildDSRetrievalSQL($data, &$joins, &$where, $andOperation = false){
-			if( preg_match('/^mysql:/i', $data[0]) ){
+        return Symphony::Database()->insert($fields, 'tbl_fields_'.$this->handle());
+    }
 
-				$field_id = $this->get('id');
+    /*-------------------------------------------------------------------------
+        UI
+    -------------------------------------------------------------------------*/
 
-				$expression = str_replace(array('mysql:', 'value'), array('', " `t$field_id`.`value` "), $data[0]);
+    /**
+     *
+     * prepare table value
+     *
+     * @since version 1.0.0
+     */
 
-				$joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id` ON (`e`.`id` = `t$field_id`.entry_id) ";
-				$where .= " AND $expression ";
-			}
+    public function displayPublishPanel(XMLElement &$wrapper, $data = NULL, $flagWithError = NULL, $fieldnamePrefix = NULL, $fieldnamePostfix = NULL, $entry_id = NULL)
+    {
+        $value = $data['value'];
+        $label = Widget::Label($this->get('label'));
 
-			else parent::buildDSRetrievalSQL($data, $joins, $where, $andOperation);
+        $label->appendChild(
+            Widget::Input(
+                'fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix,
+                (string) (strlen($value) != 0 ? $value : $this->getNewNumber()),
+                'text',
+                array('readonly' => 'readonly')
+            )
+        );
 
-			return true;
-		}
+        if( $flagWithError != NULL ) {
+            $wrapper->appendChild(Widget::Error($label, $flagWithError));
+        } else {
+            $wrapper->appendChild($label);
+        }
+    }
 
-	}
+    /**
+     *
+     * display datasource filter panel
+     *
+     * @since version 1.0.0
+     */
+
+    public function displayDatasourceFilterPanel(XMLElement &$wrapper, $data = NULL, $errors = NULL, $fieldnamePrefix = NULL, $fieldnamePostfix = NULL)
+    {
+        $wrapper->appendChild(new XMLElement('h4', $this->get('label').' <span>'.$this->Name().'</span>'));
+        $label = Widget::Label('Value');
+        $label->appendChild(
+            Widget::Input(
+                'fields[filter]'.($fieldnamePrefix ? '['.$fieldnamePrefix.']' : '').'['.$this->get('id').']'.($fieldnamePostfix ? '['.$fieldnamePostfix.']' : ''),
+                ($data ? General::sanitize($data) : NULL))
+            );
+        $wrapper->appendChild($label);
+
+        $wrapper->appendChild(new XMLElement('p', 'To filter by ranges, add <code>mysql:</code> to the beginning of the filter input. Use <code>value</code> for field name. E.G. <code>mysql: value &gt;= 1.01 AND value &lt;= {$price}</code>', array('class' => 'help')));
+    }
+
+    /*-------------------------------------------------------------------------
+        INPUT / SAVE FIELD DATA
+    -------------------------------------------------------------------------*/
+
+    /**
+     *
+     * check post field data
+     *
+     * @since version 1.0.0
+     */
+
+    public function checkPostFieldData($data, &$message, $entry_id = NULL)
+    {
+        $message = NULL;
+
+        if( $this->get('required') == 'yes' && strlen($data) == 0 ){
+            $message = 'This is a required field.';
+            return self::__MISSING_FIELDS__;
+        }
+
+        if( strlen($data) > 0 && !is_numeric($data) ){
+            $message = 'Must be a number.';
+            return self::__INVALID_FIELDS__;
+        }
+
+        return self::__OK__;
+    }
+
+    /**
+     *
+     * process raw field data
+     *
+     * @since version 1.0.0
+     */
+
+    public function processRawFieldData($data, &$status, &$message = NULL, $simulate = false, $entry_id = null)
+    {
+        if( !$data ) $data = $this->getNewNumber();
+
+        return parent::processRawFieldData($data, $status, $message, $simulate, $entry_id);
+    }
+
+    /**
+     *
+     * get new number
+     *
+     * @since version 1.0.0
+     */
+
+    public function getNewNumber()
+    {
+        $last_num = Symphony::Database()->fetch("
+            SELECT `value`
+            FROM `tbl_entries_data_".$this->get('id')."`
+            ORDER BY `value` DESC LIMIT 1
+        ");
+
+        return (int) (!empty($last_num)) ? $last_num[0]['value'] + 1 : $this->get('start_number');
+    }
+
+    /*-------------------------------------------------------------------------
+        DATA SOURCE OUTPUT
+    -------------------------------------------------------------------------*/
+
+    /**
+     *
+     * build ds retrieval sql
+     *
+     * @since version 1.0.0
+     */
+
+    function buildDSRetrievalSQL($data, &$joins, &$where, $andOperation = false)
+    {
+        if( preg_match('/^mysql:/i', $data[0]) ){
+
+            $field_id = $this->get('id');
+
+            $expression = str_replace(array('mysql:', 'value'), array('', " `t$field_id`.`value` "), $data[0]);
+
+            $joins .= " LEFT JOIN `tbl_entries_data_$field_id` AS `t$field_id` ON (`e`.`id` = `t$field_id`.entry_id) ";
+            $where .= " AND $expression ";
+        }
+
+        else parent::buildDSRetrievalSQL($data, $joins, $where, $andOperation);
+
+        return true;
+    }
+}
